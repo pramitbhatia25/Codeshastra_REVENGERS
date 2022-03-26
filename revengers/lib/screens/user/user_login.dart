6c28b7a1840d6fbad5a4c381b51c32dfe2b2dbe4
Pramit Bhatia <pramitbhatia25@gmail.com>
2022-03-26 16:03:57 +0530

    Edits

--- revengers/lib/screens/user/user_login.dart
+++ revengers/lib/screens/user/user_login.dart
@@ -21,74 +21,89 @@ class _User_LoginState extends State<User_Login> {
   Widget build(BuildContext context) {
     return Scaffold(
       resizeToAvoidBottomInset: false,
-      backgroundColor: Colors.red[400],
-      appBar: myAppBar(title: "User Login."),
+      backgroundColor: Colors.orange[100],
       body: SafeArea(
-        child: Column(
-          children: <Widget>[
-            SizedBox(height: 200),
-            const Text('codeLib 1.0',
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 40,
-                  fontFamily: 'Mogra',
-                )),
-            SizedBox(height: 40),
-            Padding(
-              padding: EdgeInsets.only(left: 40, right: 40),
-              child: TextField(
-                controller: username,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(20)),
-                  filled: true,
-                  fillColor: Colors.transparent,
-                  hintText: 'Username',
+        child: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [
+                Colors.red,
+                Colors.orangeAccent,
+                Colors.deepOrange,
+                Colors.orangeAccent,
+                Colors.red
+              ],
+            ),
+          ),
+          child: Column(
+            children: <Widget>[
+              SizedBox(height: 150),
+              const Text('Revengers',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 40,
+                    fontFamily: 'Mogra',
+                  )),
+              SizedBox(height: 40),
+              Padding(
+                padding: EdgeInsets.only(left: 40, right: 40),
+                child: TextField(
+                  controller: username,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                        // borderSide: BorderSide(width: 10, color: Colors.white),
+                        borderRadius: BorderRadius.circular(20)),
+                    filled: true,
+                    fillColor: Colors.transparent,
+                    hintText: 'Username',
+                  ),
                 ),
               ),
-            ),
-            Padding(
-              padding: EdgeInsets.only(top: 40, left: 40, right: 40),
-              child: TextField(
-                controller: password,
-                obscureText: true,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(20)),
-                  filled: true,
-                  fillColor: Colors.transparent,
-                  hintText: 'Password',
+              Padding(
+                padding: EdgeInsets.only(top: 40, left: 40, right: 40),
+                child: TextField(
+                  controller: password,
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20)),
+                    filled: true,
+                    fillColor: Colors.transparent,
+                    hintText: 'Password',
+                  ),
                 ),
               ),
-            ),
-            SizedBox(height: 50),
-            Padding(
-              padding: EdgeInsets.only(top: 40, left: 40, right: 40),
-              child: ElevatedButton(
-                  onPressed: () async {
-                    try {
-                      final user = await _auth.signInWithEmailAndPassword(
-                        email: username.text,
-                        password: password.text,
-                      );
-                      if (user != null) {
-                        Navigator.of(context).pushNamed(User_Home.routeName);
+              SizedBox(height: 50),
+              Padding(
+                padding: EdgeInsets.only(top: 40, left: 40, right: 40),
+                child: ElevatedButton(
+                    onPressed: () async {
+                      try {
+                        final user = await _auth.signInWithEmailAndPassword(
+                          email: username.text,
+                          password: password.text,
+                        );
+                        if (user != null) {
+                          Navigator.of(context).pushNamed(User_Home.routeName);
+                        }
+                      } catch (e) {
+                        print(e);
                       }
-                    } catch (e) {
-                      print(e);
-                    }
-                  },
-                  child: Text('Login')),
-            ),
-            Padding(
-              padding: EdgeInsets.only(top: 40, left: 40, right: 40),
-              child: ElevatedButton(
-                  onPressed: () {
-                    Navigator.of(context).pushNamed(User_Register.routeName);
-                  },
-                  child: Text('Register')),
-            )
-          ],
+                    },
+                    child: Text('Login')),
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 40, left: 40, right: 40),
+                child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.of(context).pushNamed(User_Register.routeName);
+                    },
+                    child: Text('Register')),
+              )
+            ],
+          ),
         ),
       ),
     );
--- revengers/lib/screens/welcome_screen.dart
+++ revengers/lib/screens/welcome_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 import 'package:revengers/screens/artist/artist_login.dart';
 import 'package:revengers/screens/user/user_login.dart';
 import 'package:revengers/widgets/roundbutton.dart';
@@ -11,36 +12,124 @@ class WelcomeScreen extends StatefulWidget {
 }
 
 class _WelcomeScreenState extends State<WelcomeScreen> {
+  TextEditingController username = new TextEditingController();
+  TextEditingController password = new TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       resizeToAvoidBottomInset: false,
       backgroundColor: Colors.red[400],
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                primary: Colors.red, // background
-              ),
-              child:
-                  Text('Enter as User', style: TextStyle(color: Colors.black)),
-              onPressed: () {
-                Navigator.of(context).pushNamed(User_Login.routeName);
-              },
+      body: SafeArea(
+        child: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [
+                Colors.red,
+                Colors.orangeAccent,
+                Colors.deepOrange,
+                Colors.orangeAccent,
+                Colors.red
+              ],
             ),
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                primary: Colors.red, // background
+          ),
+          child: Column(
+            children: [
+              SizedBox(height: 150),
+              Container(
+                height: 150,
+                width: 150,
+                child: Lottie.asset('../assets/lottie/continuePage.json',
+                    height: 150),
               ),
-              child: Text('Enter as Artist',
-                  style: TextStyle(color: Colors.black)),
-              onPressed: () {
-                Navigator.of(context).pushNamed(Artist_Login.routeName);
-              },
-            ),
-          ],
+              SizedBox(height: 150), //lottie
+              Expanded(
+                child: Container(
+                  height: 100,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                        topRight: Radius.circular(60),
+                        topLeft: Radius.circular(60)),
+                  ),
+                  child: Column(
+                    children: <Widget>[
+                      SizedBox(height: 20.0),
+                      Padding(
+                        padding: const EdgeInsets.only(
+                            top: 30.0, left: 40.0, right: 40.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.stretch,
+                          children: <Widget>[
+                            Container(
+                              width: 200.0,
+                              child: ElevatedButton(
+                                  onPressed: () {
+                                    Navigator.of(context)
+                                        .pushNamed(User_Login.routeName);
+                                  },
+                                  child: Padding(
+                                    padding: const EdgeInsets.only(
+                                        top: 15.0, bottom: 15),
+                                    child: Text('Enter As User',
+                                        style: TextStyle(
+                                          fontSize: 20.0,
+                                          fontWeight: FontWeight.bold,
+                                          color: Colors.black,
+                                          fontStyle: FontStyle.italic,
+                                        )),
+                                  ),
+                                  style: ElevatedButton.styleFrom(
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(30),
+                                    ),
+                                    primary: Colors.orange[100],
+                                  )),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(
+                            top: 30.0, left: 40.0, right: 40.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.stretch,
+                          children: <Widget>[
+                            Container(
+                              width: 200.0,
+                              child: ElevatedButton(
+                                  onPressed: () {
+                                    Navigator.of(context)
+                                        .pushNamed(Artist_Login.routeName);
+                                  },
+                                  child: Padding(
+                                    padding: const EdgeInsets.only(
+                                        top: 15.0, bottom: 15),
+                                    child: Text('Enter As Artist',
+                                        style: TextStyle(
+                                          fontSize: 20.0,
+                                          fontWeight: FontWeight.bold,
+                                          color: Colors.black,
+                                          fontStyle: FontStyle.italic,
+                                        )),
+                                  ),
+                                  style: ElevatedButton.styleFrom(
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(30),
+                                    ),
+                                    primary: Colors.orange[100],
+                                  )),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
